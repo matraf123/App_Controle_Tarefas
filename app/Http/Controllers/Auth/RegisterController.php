e16c6af20204b985d80313ed4c195edb92f9d29a
matraf123 <matraf123@gmail.com>
2022-07-27 16:54:59 -0300

    modified:   app/Http/Controllers/Auth/RegisterController.php 	new file:   app/Http/Controllers/TarefaController.php 	new file:   app/Models/Tarefa.php 	new file:   package-lock.json 	modified:   package.json 	new file:   public/css/app.css 	new file:   public/js/app.js 	new file:   public/mix-manifest.json 	modified:   resources/views/auth/login.blade.php 	modified:   resources/views/auth/register.blade.php 	modified:   routes/web.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -24,6 +24,10 @@ class RegisterController extends Controller
 
     use RegistersUsers;
 
+    public function showRegistrationForm(){
+        return view ('auth.register');
+    }
+
     /**
      * Where to redirect users after registration.
      *
